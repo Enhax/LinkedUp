d5ff1c1d0a143f7e67946b9720d0df7ad81a8de6
Franck <50842288+Enhax@users.noreply.github.com>
2023-10-08 20:21:44 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,5 @@
+// PROFILE_MENU + DARK_MODE
+
 const profileMenu = document.getElementById('profileMenu')
 const navProfileImg = document.querySelector('.nav-profile-img')
 const darkBtn = document.getElementById('dark-btn')
@@ -8,4 +10,20 @@ navProfileImg.onclick = function(){
 
 darkBtn.onclick = function(){
     darkBtn.classList.toggle('dark-btn-on')
+}
+
+// MEDIA_QUERY_ACTIVITY
+
+const sidebarActivity = document.querySelector('.sidebar-activity')
+const showMoreLink = document.getElementById('showMoreLink')
+
+showMoreLink.onclick = function(){
+    sidebarActivity.classList.toggle('show-activity')
+
+    if(sidebarActivity.classList.contains('show-activity')){
+        showMoreLink.innerHTML= 'Voir moins <b>-</b>'
+    }
+    else{
+        showMoreLink.innerHTML= 'Voir plus <b>+</b>'
+    }
 }
