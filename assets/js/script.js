3e2ff02c377eb35e1e196c9f490dba78fbb0bc4d
Franck <50842288+Enhax@users.noreply.github.com>
2023-10-08 11:48:07 +0200

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,6 +1,11 @@
 const profileMenu = document.getElementById('profileMenu')
 const navProfileImg = document.querySelector('.nav-profile-img')
+const darkBtn = document.getElementById('dark-btn')
 
 navProfileImg.onclick = function(){
     profileMenu.classList.toggle('open-menu')
+}
+
+darkBtn.onclick = function(){
+    darkBtn.classList.toggle('dark-btn-on')
 }
