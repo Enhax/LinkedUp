7e1e83b732a0c222c3be945f8eb3bfdf3f9ad67c
Franck <50842288+Enhax@users.noreply.github.com>
2023-11-26 22:15:37 +0100

    new

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,9 @@
 // PROFILE_MENU + DARK_MODE
 
-const profileMenu = document.getElementById('profileMenu')
-const navProfileImg = document.querySelector('.nav-profile-img')
-const darkBtn = document.getElementById('dark-btn')
+const profileMenu = document.getElementById('profileMenu');
+const navProfileImg = document.querySelector('.nav-profile-img');
+const darkBtn = document.getElementById('dark-btn');
+let profileMenuLink = document.getElementsByClassName('profile-menu-link');
 
 navProfileImg.onclick = function(){
     profileMenu.classList.toggle('open-menu')
@@ -11,6 +12,31 @@ navProfileImg.onclick = function(){
 darkBtn.onclick = function(){
     darkBtn.classList.toggle('dark-btn-on')
 }
+
+document.addEventListener('click', (e)=>{
+    /* console.log(e.target); */
+    if(e.target != darkBtn && e.target != darkBtn.firstChild && e.target != profileMenu && e.target != navProfileImg && e.target != document.querySelector('.user-info') && e.target != document.getElementById('inner-profile')){
+        profileMenu.classList.remove('open-menu')
+        
+        /* Array.from(profileMenuLink).forEach(link =>{
+            if(e.target != link && e.target != link.childNodes){
+                profileMenu.classList.remove('open-menu')
+            }
+        }) */
+
+        /* for(let link of profileMenuLink){
+            if(e.target != link && e.target != link.childNodes){
+                profileMenu.classList.remove('open-menu')
+            }
+        } */
+
+        /* for (let index = 0; index < profileMenuLink.length; index++){
+            if(e.target != profileMenuLink[index]){
+                profileMenu.classList.remove('open-menu')
+            }           
+        } */
+    }
+})
 
 // MEDIA_QUERY_ACTIVITY
 
